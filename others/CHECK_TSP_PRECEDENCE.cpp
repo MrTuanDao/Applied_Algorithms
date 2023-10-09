5c6cd5cc56207d1e0eaa637eda8ffc1e6b55e09d
MrTuanDao <103438257+MrTuanDao@users.noreply.github.com>
2023-10-09 10:42:52 +0700

    Create CHECK_TSP_PRECEDENCE.cpp

---
+++ others/CHECK_TSP_PRECEDENCE.cpp
@@ -0,0 +1,49 @@
+// check TSP with Precedence Constraint
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 1e3 + 10, MAXM = 1e6 + 10;
+int n, m;
+int totalMove = 0;
+int dis_matrix[MAXN][MAXN];
+int predict[MAXN], visited[MAXN];
+vector <int> precedence[MAXN];
+
+void input(){
+    cin >> n;
+    for(int i = 1; i <= n; i++) cin >> predict[i];
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++) cin >> dis_matrix[i][j];
+    }
+    cin >> m;
+    for(int i = 1; i <= m; i++){
+        int x, y; cin >> x >> y;
+        // cout << endl;
+        precedence[y].push_back(x);
+    }
+}
+
+bool goodToGo(int i){ 
+    for(auto it = precedence[i].begin(); it != precedence[i].end(); it++){
+        if (visited[*it] == 0) return false;
+    }
+    return true;
+}
+
+int main(){
+    input();
+    int lastmove = 0;
+    for(int i = 1; i <= n; i++){
+        int curmove = predict[i];
+        if(!goodToGo(curmove)){
+            cout << "-1";
+            return 0;
+        }
+        if (lastmove != 0)
+            totalMove += dis_matrix[lastmove][curmove];
+        lastmove = curmove;
+        visited[curmove] = 1;
+    }
+    cout << totalMove + dis_matrix[lastmove][predict[1]];
+    return 0;
+}
