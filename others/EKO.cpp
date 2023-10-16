9072b150f88a9134f01d5d8b26cf7494e2972eb9
MrTuanDao <103438257+MrTuanDao@users.noreply.github.com>
2023-10-16 10:38:00 +0700

    Update EKO.cpp

--- others/EKO.cpp
+++ others/EKO.cpp
@@ -5,16 +5,15 @@ Có thể thực hiện cắt ở độ cao h với tất cả các cây
 Số lượng gỗ thu được là phần chóp của các cây cao hơn hơn h
 Tìm h lớn nhất có thể để số lượng gỗ thu được tối thiểu là m
 */
-
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-const int MAXN = 1e6;
+const int MAXN = 1e6 + 10;
 int n, m;
 int heights[MAXN];
 
-int amount(int h){
-    int totalWood = 0;
+long long amount(int h){
+    long long totalWood = 0;
     for(int i = 1; i <= n; i++){
         if(heights[i] > h) totalWood += heights[i] - h;
     }
@@ -22,8 +21,10 @@ int amount(int h){
 }
 
 int main(){
+    ios_base::sync_with_stdio(false); cin.tie(0);
     cin >> n >> m;
     int max_h = 0;
+    
     for(int i = 1; i <= n; i++){
         cin >> heights[i];
         max_h = max(max_h, heights[i]);
@@ -31,10 +32,10 @@ int main(){
     int lo = 0, hi = max_h;
     while(lo < hi){
         int mid = (lo + hi) / 2;
-        if(amount(mid) > m) lo = mid + 1;
+        if(amount(mid) >= m) lo = mid;
         else hi = mid - 1;
     }
-    
-    cout << lo;
+    if(amount(hi) >= m)) cout << hi << endl;
+    else cout << lo;
     return 0;
 }
