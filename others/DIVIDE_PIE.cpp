3b8aaacadeea21b1fc22cf4781ef557446514447
MrTuanDao <103438257+MrTuanDao@users.noreply.github.com>
2023-10-16 08:43:11 +0700

    Create DIVIDE_PIE.cpp

---
+++ others/DIVIDE_PIE.cpp
@@ -0,0 +1,51 @@
+/*
+Bai toan chia banh 
+Tai sao day la bai toan Binary Search
+P(x) đúng nếu chia đươc
+Đ Đ Đ Đ ... Đ S S S ... S
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 10000;
+const double PI = M_PI;
+
+double EPS = 1e-4,
+       lo, hi;
+int n, f;
+double min_area = DBL_MAX;
+double area[MAXN];
+
+bool P(double x){ // neu chia duoc so banh cho F + 1 nguoi thi la DUNG
+    int sumPies = 0;
+    for(int i = 1; i <= n; i++){
+        int pies = area[i]/x;
+        sumPies += pies;
+    }
+    if(sumPies >= f + 1) return true;
+    return false;
+}
+
+int main(){
+
+    cin >> n >> f;
+    for(int i = 1; i <= n; i++) {
+        int x;
+        cin >> x; area[i] = PI * x * x;
+        min_area = min(min_area, area[i]);
+    }
+    
+    lo = 0; hi = min_area;
+    while(hi - lo > EPS){
+        double mid = (lo + hi) / 2.0;
+        if (P(mid) == true){
+            lo = mid;
+        } else {
+            hi = mid;
+        }
+    }
+    
+    printf("%.4f", lo);
+    
+    return 0;
+}
