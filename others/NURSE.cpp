baa727e06a06d283ed293f4f94adf3642f9ff9bb
MrTuanDao <103438257+MrTuanDao@users.noreply.github.com>
2023-10-29 15:43:43 +0700

    Update NURSE.cpp

--- others/NURSE.cpp
+++ others/NURSE.cpp
@@ -5,10 +5,12 @@ const int N = 1e3 + 10;
 int n, k1, k2;
 int dp[N];
 
+// Trả về Số lượng cách cho h ngày thoả mãn
+// 1.bắt đầu bằng 1 chuỗi ngày
+// 2.sau 1 chuỗi ngày luôn là 1 ngày nghỉ
 int count(int h) {
     if (h == 0) return dp[h] = 1;
     if ( h < k1 + 1) return 0;
-    
     if (dp[h]) return dp[h];
     
     int res = 0;
@@ -18,6 +20,7 @@ int count(int h) {
     return dp[h] = res;
 }
 
+// Trả về Số lượng cách cho k ngày thoả mãn bắt đầu bằng 1 chuỗi làm việc
 int solve(int k) {
     int res = count(k);
     for (int i = k1; i <= k2; i++){
