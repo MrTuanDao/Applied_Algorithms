ae8ac39bf7ee83d110a20154f2004c173e0e6573
MrTuanDao <103438257+MrTuanDao@users.noreply.github.com>
2023-10-30 23:42:03 +0700

    Create MAX_EVEN_cusum.cpp

---
+++ Thuc_hanh_buoi_4/MAX_EVEN_cusum.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+const int INF = 1e12;
+const int N = 2e6;
+int a[N];
+int pre[N] = {0}, minLe = 1e17 + 1, minChan = 0; // min chẵn bằng 0 vì lần đầu tiên xét đến pre[i] chẵn ta lấy luôn số đó
+int res = 0;
+
+void solve () {
+    int n; cin >> n; 
+
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        
+        pre[i] = pre[i - 1] + a[i];
+      
+        if ( (pre[i] + INF) % 2 == 0 ) {
+            res = max(res, pre[i] - minChan);
+            minChan = min(minChan, pre[i]);
+        }
+        else {
+            res = max(res, pre[i] - minLe);
+            minLe = min(minLe, pre[i]);
+        }
+    }
+  
+    cout << res << endl;
+}
+
+signed main() {
+    solve();
+    return 0;
+}
