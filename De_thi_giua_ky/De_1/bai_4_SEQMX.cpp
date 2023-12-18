5a245799d06dcbc7997dd5f3a01e039fc0690e71
MrTuanDao <103438257+MrTuanDao@users.noreply.github.com>
2023-12-18 10:15:30 +0700

    Update bai_4_SEQMX.cpp

--- De_thi_giua_ky/De_1/bai_4_SEQMX.cpp
+++ De_thi_giua_ky/De_1/bai_4_SEQMX.cpp
@@ -1,16 +1,18 @@
-#include <bits/stdc++.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
 using namespace std;
 
+const double INF = 1e18;
 const int MAX_N = 1e5 + 10;
 int n, k;
-int pre_cal[MAX_N];
+long long pre_cal[MAX_N];
 
 
 void input() {
     cin >> n >> k;
     pre_cal[0] = 0;
     cin >> pre_cal[1];
-    int x;
+    long long x;
     for (int i = 2; i <= n; i++) {
         cin >> x;
         pre_cal[i] = pre_cal[i - 1] + x;
@@ -18,28 +20,37 @@ void input() {
 }
 
 double TRY(int j) { //tim trung binh cong lon nhat cua day con co j phan tu
-    double max_tb = 0;
-    
+    long long max_tb = - INF;
+
     for (int m = 1; m <= n - j + 1; m++) {
-        max_tb = max(max_tb, (pre_cal[m + j - 1] - pre_cal[m - 1]) * 1.0 / j);
+        max_tb = max(max_tb, (pre_cal[m + j - 1] - pre_cal[m - 1]));
+        //if (max_tb == 0) cout << "aha" << endl;
     }
-    
-    return max_tb;
-    
+    return max_tb * 1.0 / j;
+
 }
 
 double solve() {
-    double ketqua = 0;
+    double ketqua = -INF;
     for (int i = k; i <= n; i++) {
-        ketqua = max(ketqua, TRY(i));
+        long long max_tb = -INF;
+
+        for (int m = 1; m <= n - i + 1; m++) {
+            max_tb = max(max_tb, (pre_cal[m + i - 1] - pre_cal[m - 1]));
+            //if (max_tb == 0) cout << "aha" << endl;
+        }
+
+        ketqua = max(ketqua, max_tb * 1.0 / i);
     }
     return ketqua;
 }
 
 int main() {
+    //freopen("data.in", "r", stdin);
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
     input();
     double ketqua = solve();
-    
+
     printf("%.5lf", ketqua);
-    
+
 }
